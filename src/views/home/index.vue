16048c02952ab46864c8162552ec71c7c89cc92a
aizexin <443792431@qq.com>
2021-09-11 21:40:38 +0800

    commit 1 头部导航栏

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,13 +1,33 @@
 <template>
-    <div>
-
-    </div>
+  <div class="home-container">
+    <!-- 导航栏 -->
+    <van-nav-bar class="page-nav-bar">
+      <template v-slot:title>
+        <van-button
+          class="search-btn"
+          type="primary"
+          size="small"
+          round
+          icon="search"
+          >搜索</van-button
+        >
+      </template>
+    </van-nav-bar>
+    <!-- /导航栏 -->
+  </div>
 </template>
 
 <script setup lang="ts">
-
 </script>
 
-<style scoped lang="less">
-
+<style lang="less" scoped>
+.home-container {
+  .search-btn {
+    width: 555px;
+    height: 64px;
+    background-color: #5babfb;
+    border: none;
+    font-size: 28px;
+  }
+}
 </style>
