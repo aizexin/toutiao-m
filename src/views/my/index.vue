35577b43b4d1c70113faf45b069ff3e51812a768
aizexin <443792431@qq.com>
2021-09-11 11:32:49 +0800

    grid nav

--- src/views/my/index.vue
+++ src/views/my/index.vue
@@ -1,43 +1,152 @@
 <template>
-    <div class="my-container">
-        <div class="header not-login">
-            <div class="login-btn" @click="$router.push('/login')">
-                <img class="mobile-image" src="./mobile.png">
-                <span class="text">登录 / 注册</span>
-            </div>
-        </div>
+  <div class="my-container">
+    <div class="header not-login">
+      <div class="login-btn" @click="$router.push('/login')">
+        <img class="mobile-image" src="./mobile.png" />
+        <span class="text">登录 / 注册</span>
+      </div>
     </div>
+    <div class="header user-info">
+      <div class="base-info">
+        <div class="left">
+          <van-image
+            class="avatar"
+            round
+            fit="cover"
+            src="https://img01.yzcdn.cn/vant/cat.jpeg"
+          />
+          <span class="name">头条号</span>
+        </div>
+        <div class="right">
+          <van-button round size="mini">编辑资料</van-button>
+        </div>
+      </div>
+      <div class="data-stats">
+        <div class="data-item">
+          <span class="count">10</span>
+          <span class="text">头条</span>
+        </div>
+        <div class="data-item">
+          <span class="count">10</span>
+          <span class="text">关注</span>
+        </div>
+        <div class="data-item">
+          <span class="count">10</span>
+          <span class="text">粉丝</span>
+        </div>
+        <div class="data-item">
+          <span class="count">10</span>
+          <span class="text">获赞</span>
+        </div>
+      </div>
+      <!-- 导航 -->
+      <van-grid class="grid-nav" :column-num="2" clickable >
+        <van-grid-item class="grid-item" icon="photo-o" text="收藏">
+          <template v-slot:icon>
+            <i class="toutiao toutiao-shoucang"></i>
+          </template>
+        </van-grid-item>
+        <van-grid-item class="grid-item" icon="photo-o" text="历史">
+          <template v-slot:icon>
+            <i class="toutiao toutiao-lishi"></i>
+          </template>
+        </van-grid-item>
+      </van-grid>
+      <!--/ 导航 -->
+    </div>
+  </div>
 </template>
 
 <script setup lang="ts">
-
 </script>
 
 <style scoped lang="less">
 .my-container {
-    .header{
-        height: 361px;
-        background: url("./banner.png");
-        background-size: cover;
+  .header {
+    height: 361px;
+    background: url('./banner.png');
+    background-size: cover;
+  }
+  .not-login {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    .login-btn {
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+      .mobile-image {
+        width: 132px;
+        height: 132px;
+        margin-bottom: 15px;
+      }
+      .text {
+        font-size: 28px;
+        color: white;
+      }
     }
-    .not-login {
+  }
+  .user-info {
+    .base-info {
+      height: 231px;
+      padding: 76px 32px 23px;
+      // 让height 把padding计算在其中
+      box-sizing: border-box;
+      display: flex;
+      justify-content: space-between;
+      align-items: center;
+      .left {
         display: flex;
-        justify-content: center;
         align-items: center;
-        .login-btn {
-            display: flex;
-            flex-direction: column;
-            align-items: center;
-            .mobile-image {
-                width: 132px;
-                height: 132px;
-                margin-bottom: 15px;
-            }
-            .text {
-                font-size: 28px;
-                color: white;
-            }
+        .avatar {
+          height: 132px;
+          width: 132px;
+          margin-right: 23px;
+          border: 4px solid white;
         }
+        .name {
+          font-size: 30px;
+          color: white;
+        }
+      }
     }
+    .data-stats {
+      display: flex;
+      .data-item {
+        height: 130px;
+        flex: 1;
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        justify-content: center;
+        color: white;
+        .count {
+          font-size: 36px;
+        }
+        .text {
+          font-size: 23px;
+          margin-top: 9px;
+        }
+      }
+    }
+    .grid-nav {
+      .grid-item {
+        height: 141px;
+        i.toutiao {
+          font-size: 45px;
+        }
+        .toutiao-shoucang {
+          color: #eb5253;
+        }
+        .toutiao-lishi {
+          color: #ff9d1d;
+        }
+
+        span.text {
+          font-size: 28px;
+        }
+      }
+    }
+  }
 }
 </style>
