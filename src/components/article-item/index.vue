4e8a5064ac657c48d85c542af09b0a1d714a385b
aizexin <443792431@qq.com>
2021-09-14 14:41:24 +0800

    调整cell

--- src/components/article-item/index.vue
+++ src/components/article-item/index.vue
@@ -1,7 +1,36 @@
 <template>
   <van-cell class="article-item">
+    <!-- 标题 -->
     <template v-slot:title>
-      <div>xxx {{ JSON.stringify(item) }}</div>
+      <div class="title van-multi-ellipsis--l2">{{ item.title }}</div>
+    </template>
+    <!-- /标题 -->
+    <template v-slot:label>
+      <div v-if="item.cover.type === 3" class="cover-wrap">
+        <div
+          class="cover-item"
+          v-for="(image, index) in item.cover.images"
+          :key="index"
+        >
+          <van-image
+            class="cover-item-img"
+            fit="cover"
+            :src="item.cover.images[0]"
+          />
+        </div>
+      </div>
+      <div class="label-info-wrap">
+        <span>{{ item.aut_name }}</span>
+        <span>{{ item.comm_count }}评论</span>
+        <span>{{ item.pubdate }}</span>
+      </div>
+    </template>
+    <template v-if="item.cover.type === 1" v-slot:value>
+      <van-image
+        class="right-cover"
+        fit="cover"
+        :src="item.cover.images[0]"
+      />
     </template>
   </van-cell>
 </template>
@@ -24,5 +53,38 @@ export default {
 
 <style lang="less" scoped>
 .article-item {
+  .title {
+    color: #3a3a3a;
+    font-size: 32px;
+  }
+  .van-cell__value {
+    flex: unset;
+    width: 232px;
+    height: 146px;
+  }
+  .label-info-wrap span {
+    font-size: 23px;
+    color: #b4b4b4;
+    margin-right: 25px;
+  }
+  //   三个图片的样式
+  .cover-wrap {
+    display: flex;
+    padding: 30px 0px;
+    .cover-item {
+      flex: 1;
+      height: 146px;
+      // &表示父元素本身
+      &:not(:last-child) {
+        padding-right: 4px;
+      }
+      .cover-item-img {
+        width: 100%;
+        height: 146px;
+      }
+    }
+  }
+  .right-cover {
+  }
 }
 </style>
